db199766406840dae881b4b707555b3083cc8cd1
panjunjie <panjunjie8611@gmail.com>
2022-07-03 20:51:32 +0800

    fundamental and string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <QtCore/QDebug>
+#include <iostream>
+#include <cctype>
 
 int main()
 {
     qDebug() << "Finally I run the cpp with QT libs..";
+    int a;
+    qDebug() << a;
+
+    // Loop
+    std::string word = "hello";
+    for (int letterIndex = 0; letterIndex < word.length(); letterIndex++)
+    {
+        std::cout << word[letterIndex] << std::endl;
+    }
+
+    for (char letter : word)
+    {
+        std::cout << letter << std::endl;
+    }
+
+    // c functions
+    char lowChar = 'n';
+    std::cout << isalpha(lowChar) << std::endl;
 }
